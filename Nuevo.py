931689a96b3206dc6624c985d6f1dbdb65011350
DON GATO <camilocondoriano@gmail.com>
2026-01-29 14:35:55 -0500

    resolviendo problemas 2

--- Nuevo.py
+++ Nuevo.py
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 nombre = "almirante"
 =======
 nombre = "comandante"
 >>>>>>> e68eabbcc0 (listo)
+=======
+nombre = "comandantes"
+>>>>>>> 9d1461f103 (resolviendo problemas)
 apellido = "shepard"
 saludo = f"Hola {nombre} {apellido}, ¿como estas?"
 print(saludo)
