0e11f0f8df3b89175b562f12c59024c3e69aebdc
condoriano cursor <camilocondoriano@gmail.com>
2026-01-29 14:40:59 -0500

    problema Nuevo.py

--- Nuevo.py
+++ Nuevo.py
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 nombre = "almirante"
-=======
-nombre = "comandante"
->>>>>>> e68eabbcc0 (listo)
-=======
-nombre = "comandantes"
->>>>>>> 9d1461f103 (resolviendo problemas)
 apellido = "shepard"
 saludo = f"Hola {nombre} {apellido}, ¿como estas?"
 print(saludo)
+
 
 
